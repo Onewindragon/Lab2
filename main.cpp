09617fd2e976cbedf11881aa24820fb219572af6
siu <song820518@snu.ac.kr>
2024-06-08 05:57:26 +0000

    modify func

--- main.cpp
+++ main.cpp
@@ -1,19 +1,26 @@
 #include<iostream>
 #include<vector>
-
 using namespace std;
-
 int N,M;
 vector<int> vec;
-
+int k = 0;
 void func(int cnt) {
-
-
-
-
-
-
-
+    k++;
+    if(cnt == M){
+        k--;
+        for(int i = 0; i < M; i++){
+            cout << vec[i] << " ";
+        }
+        cout << endl;
+        return;
+    }
+    vec.push_back(k);
+    func(cnt+1);
+    vec.pop_back();
+    if(k-cnt <= N-M){
+    func(cnt);
+    }
+    k--;
 }
 
 int main() {
